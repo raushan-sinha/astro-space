b850e3aa0ca31af13f5c5a3cc2f18ae955ec525f
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-02-18 20:45:42 +0530

    Added logic for Menu open & Close for small devices

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,3 +1,6 @@
+import { Close, Menu } from "@mui/icons-material";
+import { useState } from "react";
+
 // import React from 'react'
 interface NavItem {
     name: string,
@@ -12,23 +15,51 @@ const navLinks: NavItem[] = [
 
 const Navbar = () => {
     const title: string = 'AstroSpace';
+    const [open, setOpen] = useState<boolean>(false);
 
     return (
-        <nav className='w-[50%] px-4 flex flex-row justify-between items-center bg-[#101923] rounded-xl m-auto fixed top-4 left-1/2 -translate-x-1/2 border-cyan-500 border-2'>
+        <nav className='w-[90%] md:w-[70%] lg:w-[50%] px-4 py-3 md:py-0 flex justify-between items-center bg-[#101923] rounded-xl fixed top-4 left-1/2 -translate-x-1/2 border-cyan-500 border-2 relative'>
+
+            {/* Logo */}
             <div>
                 <h1 className='font-mono text-2xl text-white'>
                     {title.split('').map((char, idx) => (
-                        <span className={`${idx > 4 ? 'text-yellow-400' : ''}`}>{char}</span>
+                        <span key={idx} className={`${idx > 4 ? 'text-yellow-400' : ''}`}>
+                            {char}
+                        </span>
                     ))}
                 </h1>
             </div>
-            <div className="flex items-center gap-10 px-8 py-4 text-white">
+
+            {/* Links - Hidden Everywhere Above Mobile */}
+            <div className="hidden md:flex items-center gap-10 px-8 py-4 text-white">
                 {navLinks.map((link, idx) => (
-                    <a key={idx} className='cursor-pointer hover:text-yellow-400 hover:underline underline-offset-4 transition font-mono text-xl'>
+                    <a
+                        key={idx}
+                        className='cursor-pointer hover:text-yellow-400 hover:underline underline-offset-4 transition font-mono text-xl'
+                    >
                         {link.name}
                     </a>
                 ))}
             </div>
+
+            {/* Menu Icon - Visible Only */}
+            <button className="block md:hidden" onClick={() => setOpen(!open)}>
+                {open ? <Close className="text-cyan-500 cursor-pointer" fontSize="large" /> : <Menu className="text-cyan-500 cursor-pointer" fontSize="large" />}
+            </button>
+
+            {/* Menu box for small devices */}
+            {open && (
+                <div className="absolute top-full mt-4 left-0 w-full bg-[#101923] border border-cyan-500 rounded-2xl shadow-lg p-6 flex flex-col gap-6 md:hidden">
+                    {navLinks.map((link, idx) => (
+                        <a
+                            key={idx}
+                            className='cursor-pointer text-white hover:text-yellow-400 hover:underline underline-offset-4 transition font-mono text-xl' onClick={() => setOpen(false)}>
+                            {link.name}
+                        </a>
+                    ))}
+                </div>
+            )}
         </nav>
     )
 }
