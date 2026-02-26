e350ff9622387cd198afab3148be9a3276cd9690
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-02-26 12:45:31 +0530

    Added pages Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { BrowserRouter, Route, Routes, } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import Feedback from "./pages/Feedback";
 
 const App: React.FC = () => {
     return (
         <main>
-            <header>
-                <Navbar />
-            </header>
+            <BrowserRouter>
+                <header>
+                    <Navbar />
+                </header>
+
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/blog" element={<Blog />} />
+                    <Route path="/feedback" element={<Feedback />} />
+                </Routes>
+            </BrowserRouter>
         </main>
     );
 };
