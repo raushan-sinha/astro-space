b33418ae6e9b9d31106d8ad0d12b9946a631b31f
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-02 15:36:11 +0530

    Deleted a Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes, } from "react-router-dom";
 import Blog from "./pages/Blog/Blog";
-import Feedback from "./pages/Feedback";
 import Navbar from "./components/layout/Navbar";
 import Home from "./pages/Home/Home";
 
@@ -15,7 +14,6 @@ const App: React.FC = () => {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/blog" element={<Blog />} />
-                    <Route path="/feedback" element={<Feedback />} />
                 </Routes>
             </BrowserRouter>
         </main>
