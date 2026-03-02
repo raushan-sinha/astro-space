a36ea59a65349d44fdb276bd721091efe83c6da5
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-02 15:39:32 +0530

    code updated

--- src/pages/Home/sections/Image.tsx
+++ src/pages/Home/sections/Image.tsx
@@ -8,6 +8,7 @@ const Image = ({ src, alt }: ImageProps) => {
         <img
             src={src}
             alt={alt}
+            loading="lazy"
             className="w-full h-56 sm:h-72 lg:h-80 object-cover object-center transition-transform duration-500 hover:scale-105"
         />
     );
